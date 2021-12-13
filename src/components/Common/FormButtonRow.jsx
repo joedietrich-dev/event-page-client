b6e2f2a359d67e68c1e4437e7659c5b55fe7103e
joedietrich-dev <joe@joedietrich.net>
2021-12-12 21:07:29 -0500

    Add props to FormButtonRow

--- src/components/Common/FormButtonRow.jsx
+++ src/components/Common/FormButtonRow.jsx
@@ -3,13 +3,13 @@ import Button from "@mui/material/Button";
 import Publish from "@mui/icons-material/Publish";
 import Delete from "@mui/icons-material/Delete";
 
-function FormButtonRow() {
+function FormButtonRow({ onSubmit = (f) => f, onDelete = (f) => f }) {
   return (
     <Grid container direction="row" justifyContent="space-between">
-      <Button variant="contained" margin="normal" startIcon={<Publish />} onClick={(f) => f}>
+      <Button variant="contained" margin="normal" startIcon={<Publish />} onClick={onSubmit}>
         Submit
       </Button>
-      <Button variant="outlined" color="warning" margin="normal" startIcon={<Delete />} onClick={(f) => f}>
+      <Button variant="outlined" color="warning" margin="normal" startIcon={<Delete />} onClick={onDelete}>
         Delete
       </Button>
     </Grid>
