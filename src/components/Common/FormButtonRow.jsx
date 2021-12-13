4429699997e382fce962c349a08bd81e64f1c99d
joedietrich-dev <joe@joedietrich.net>
2021-12-12 22:44:50 -0500

    Add option to remove delete

--- src/components/Common/FormButtonRow.jsx
+++ src/components/Common/FormButtonRow.jsx
@@ -3,15 +3,17 @@ import Button from "@mui/material/Button";
 import Publish from "@mui/icons-material/Publish";
 import Delete from "@mui/icons-material/Delete";
 
-function FormButtonRow({ onSubmit = (f) => f, onDelete = (f) => f }) {
+function FormButtonRow({ onSubmit = (f) => f, hasDelete = true, onDelete = (f) => f }) {
   return (
     <Grid container direction="row" justifyContent="space-between">
       <Button variant="contained" margin="normal" startIcon={<Publish />} onClick={onSubmit}>
         Submit
       </Button>
-      <Button variant="outlined" color="warning" margin="normal" startIcon={<Delete />} onClick={onDelete}>
-        Delete
-      </Button>
+      {hasDelete && (
+        <Button variant="outlined" color="warning" margin="normal" startIcon={<Delete />} onClick={onDelete}>
+          Delete
+        </Button>
+      )}
     </Grid>
   );
 }
