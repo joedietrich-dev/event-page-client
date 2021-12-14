f97aac42d68d397c6dc232d23aebccf00e1fc4ff
joedietrich-dev <joe@joedietrich.net>
2021-12-13 22:49:59 -0500

    Improve Sponsor List

--- src/components/Sponsor/SponsorList.jsx
+++ src/components/Sponsor/SponsorList.jsx
@@ -1,5 +1,6 @@
-import { Typography } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
 
 function SponsorList({ sponsors }) {
   return (
@@ -7,15 +8,34 @@ function SponsorList({ sponsors }) {
       <Typography variant="h3" component="h1">
         View All Sponsors
       </Typography>
-      <ul>
-        {sponsors.map((sponsor) => (
-          <li key={sponsor.id}>
-            <img src={sponsor.logo_src} alt={sponsor.name} width="100" />
-            {sponsor.name}
-            <Link to={`/sponsors/${sponsor.id}/edit`}>Edit</Link>
-          </li>
-        ))}
-      </ul>
+      <Paper elevation={1}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Logo</TableCell>
+              <TableCell align="center">Edit</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sponsors.map((sponsor) => (
+              <TableRow key={sponsor.id}>
+                <TableCell>{sponsor.name}</TableCell>
+                <TableCell>
+                  <img src={sponsor.logo_src} alt={sponsor.name} width="100" />
+                </TableCell>
+                <TableCell align="center">
+                  <Link to={`${sponsor.id}/edit`}>
+                    <Button>
+                      <EditIcon />
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     </>
   );
 }
