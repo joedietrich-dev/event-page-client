8b8eb368a87031a314d6d638e33f17c7f11e3be9
joedietrich-dev <joe@joedietrich.net>
2021-12-16 21:18:05 -0500

    Create New Panel form

--- src/components/Common/BasicSelect.jsx
+++ src/components/Common/BasicSelect.jsx
@@ -5,7 +5,7 @@ import Select from "@mui/material/Select";
 
 export default function BasicSelect({ name, label, options = [{ value: "0", label: "Select" }], value, onChange }) {
   return (
-    <FormControl fullWidth>
+    <FormControl fullWidth margin="normal">
       <InputLabel id={`${name}-label`}>{label}</InputLabel>
       <Select labelId={`${name}-label`} id={name} name={name} value={value} label={label} onChange={onChange}>
         {options.map(({ value, label }) => (
--- src/components/Panel/PanelNew.jsx
+++ src/components/Panel/PanelNew.jsx
@@ -1,5 +1,38 @@
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import TextField from "@mui/material/TextField";
+import FormContainer from "../Common/FormContainer";
+import TextFieldFixedLabel from "../Common/TextFieldFixedLabel";
+import BasicSelect from "../Common/BasicSelect";
+import useFetch from "../../hooks/useFetch";
+import useNewForm from "../../hooks/useNewForm";
+
+const blankPanel = { title: "", description: "", time: Date.now(), panelists: [], event_id: "", sponsors: [] };
+
 function PanelNew() {
-  return <div></div>;
+  const { data: events } = useFetch(`${process.env.REACT_APP_API_ROOT}/events`, []);
+  const { data: panel, handleChange, handleSubmit } = useNewForm(blankPanel, "panels");
+
+  const eventOptions = events.map((event) => ({ value: event.id, label: event.title }));
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <FormContainer title="New Panel" onSubmit={handleSubmit} hasDelete={false}>
+        <TextFieldFixedLabel name="title" label="Title" value={panel.title} onChange={handleChange} />
+        <DateTimePicker
+          renderInput={(props) => <TextField {...props} fullWidth margin="normal" />}
+          label="Date and Time"
+          id="time"
+          name="time"
+          value={panel.time}
+          onChange={(date) => handleChange({ target: { name: "time", value: date } })}
+        />
+        <BasicSelect name="event_id" label="Event" options={eventOptions} value={panel.event_id} onChange={handleChange} />
+        <TextFieldFixedLabel name="description" label="Description" value={panel.description} onChange={handleChange} multiline />
+      </FormContainer>
+    </LocalizationProvider>
+  );
 }
 
 export default PanelNew;
