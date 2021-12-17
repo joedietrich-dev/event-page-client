12fcc9d687bfb90fc236a5156231b409a6a407b4
joedietrich-dev <joe@joedietrich.net>
2021-12-16 20:33:26 -0500

    Refactor Sponsor List

--- src/components/Sponsor/SponsorList.jsx
+++ src/components/Sponsor/SponsorList.jsx
@@ -1,40 +1,11 @@
-import { Button, Paper, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
-import { Link } from "react-router-dom";
-import EditIcon from "@mui/icons-material/Edit";
 import PageHeader from "../Common/PageHeader";
+import SponsorSummaryList from "./SponsorSummaryList";
 
 function SponsorList({ sponsors }) {
   return (
     <>
       <PageHeader text="View All Sponsors" />
-      <Paper elevation={1}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Logo</TableCell>
-              <TableCell align="center">Edit</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {sponsors.map((sponsor) => (
-              <TableRow key={sponsor.id}>
-                <TableCell>{sponsor.name}</TableCell>
-                <TableCell>
-                  <img src={sponsor.logo_src} alt={sponsor.name} width="100" />
-                </TableCell>
-                <TableCell align="center">
-                  <Link to={`${sponsor.id}/edit`}>
-                    <Button>
-                      <EditIcon />
-                    </Button>
-                  </Link>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+      <SponsorSummaryList sponsors={sponsors} />
     </>
   );
 }
